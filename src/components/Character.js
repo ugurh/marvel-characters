8637af902febfcf3934449001ed191ff792bf6a3
hrnugr0000 <ugur.harun@yandex.com>
2020-01-27 20:07:40 +0300

    first commit

---
+++ src/components/Character.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+ class Character extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default Character;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+ class SearchBar extends Component {
+    render() {
+        return (
+            <div>
+                <p>Seacrh Bar</p>
+            </div>
+        )
+    }
+}
+export default  SearchBar;
