ee84227707e8a23b6cc79aec87e3f04ae386948a
hrnugr0000 <ugur.harun@yandex.com>
2020-01-28 02:42:41 +0300

    Updates

---
+++ precache-manifest.3c72153e65b29e2c2ddd6bbc55da61f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbb98974f80da46fa99614014ef39f7a",
+    "url": "/marvel-characters/index.html"
+  },
+  {
+    "revision": "960cff8eb073823cfe99",
+    "url": "/marvel-characters/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "ab67a5b96873fc416771",
+    "url": "/marvel-characters/static/css/main.10b3e9ee.chunk.css"
+  },
+  {
+    "revision": "960cff8eb073823cfe99",
+    "url": "/marvel-characters/static/js/2.a0138431.chunk.js"
+  },
+  {
+    "revision": "6f693e38c1e9f73a5316609a9e78d640",
+    "url": "/marvel-characters/static/js/2.a0138431.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab67a5b96873fc416771",
+    "url": "/marvel-characters/static/js/main.6ef026e1.chunk.js"
+  },
+  {
+    "revision": "35f0625916eb60ca8514",
+    "url": "/marvel-characters/static/js/runtime-main.2f7229d2.js"
+  }
+]);
